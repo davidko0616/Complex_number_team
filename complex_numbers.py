00f8e518bea90025ab904bd77e8e57c50cede488
Yaejun Chung <chunglaos3@gmail.com>
2025-05-01 11:21:34 +0900

    negation added

--- complex_numbers.py
+++ complex_numbers.py
@@ -8,7 +8,7 @@ class Complex:
         new_imaginary = self.imaginary + operand.imaginary
         return Complex(new_real, new_imaginary)
     
-    def __subtract__(self, operand):
+    def __sub__(self, operand):
         new_real = self.real - operand.real
         new_imaginary = self.imaginary - operand.imaginary
         return Complex(new_real, new_imaginary)
@@ -17,11 +17,18 @@ class Complex:
         new_real = (self.real*operand.imaginary)-(self.real*operand.imaginary)
         new_imaginary = (self.real*operand.imaginary)+(self.real+operand.imaginary)
         return Complex(new_real, new_imaginary)
+    
+    def __neg__(self):
+        new_real = -(self.real)
+        new_imaginary = -(self.imaginary)
+        return Complex(new_real, new_imaginary)
 
     def __str__(self):
         return f"{self.real} + {self.imaginary}i"
 
 A=Complex(1,2)
 B=Complex(4,5)
-result=A*B
-print(result)
+print(A*B)
+print(A+B)
+print(A-B)
+print(-A)
